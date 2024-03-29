6eaf8ede6c9690c83f590ff07e1b4264104ebbfe
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-29 13:50:14 +0100

    add Prodotto class with private attributes and full constructor

---
+++ src/org/experis/learning/javaShop/Prodotto.java
@@ -0,0 +1,21 @@
+package org.experis.learning.javaShop;
+
+public class Prodotto {
+    // Attributes
+    private int productCode;
+    private int productName;
+    private int productDescription;
+    private int productPrice;
+    private int iva;
+
+    // Constructors
+    public Prodotto(int productCode, int productName, int productDescription, int productPrice, int iva) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+        this.iva = iva;
+    }
+
+    // Class Methods
+}
