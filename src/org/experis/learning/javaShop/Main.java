bcd142b03686a77611dbded1f522bdf4bd05ecc6
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-29 14:46:18 +0100

    add all print in Main class

--- src/org/experis/learning/javaShop/Main.java
+++ src/org/experis/learning/javaShop/Main.java
@@ -2,13 +2,21 @@ package org.experis.learning.javaShop;
 
 public class Main {
     public static void main(String[] args) {
-        Prodotto prodotto = new Prodotto("gigi","afsfafafasf",10,2);
+        Prodotto prodotto = new Prodotto("gigi","afsfafafasf",10,20);
+        Prodotto prodotto2 = new Prodotto("alberto","blablbla",50,2);
 
-        System.out.println(prodotto.getName());
-        System.out.println(prodotto.getCode());
-        System.out.println(prodotto.getDescription());
-        System.out.println(prodotto.getPrice());
-        System.out.println(prodotto.getIvaPrice());
-        System.out.println(prodotto.getFullName());
+        System.out.println("Prodotto 1 " + prodotto.getName());
+        System.out.print("|| NOME " + prodotto.getCode());
+        System.out.print("|| DESCRIZIONE " + prodotto.getDescription());
+        System.out.print("|| PREZZO " + prodotto.getPrice());
+        System.out.print("|| PREZZO IVATO " + prodotto.getIvaPrice());
+        System.out.print("|| FULL-NAME " + prodotto.getFullName());
+
+        System.out.println("Prodotto 2 " + prodotto2.getName());
+        System.out.print("|| NOME " + prodotto2.getCode());
+        System.out.print("|| DESCRIZIONE " + prodotto2.getDescription());
+        System.out.print("|| PREZZO " + prodotto2.getPrice());
+        System.out.print("|| PREZZO IVATO " + prodotto2.getIvaPrice());
+        System.out.print("|| FULL-NAME " + prodotto2.getFullName());
     }
 }
