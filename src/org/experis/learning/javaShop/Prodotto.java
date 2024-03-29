117539926c36f1b672e770b6c5c0ae365ea12d63
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-29 13:53:28 +0100

    fix Prodotto dataType

--- src/org/experis/learning/javaShop/Prodotto.java
+++ src/org/experis/learning/javaShop/Prodotto.java
@@ -3,12 +3,16 @@ package org.experis.learning.javaShop;
 public class Prodotto {
     // Attributes
     private int productCode;
-    private int productName;
-    private int productDescription;
+    private String productName;
+    private String productDescription;
     private int productPrice;
     private int iva;
 
     // Constructors
+
+
+
+    // Constructor to set
     public Prodotto(int productCode, int productName, int productDescription, int productPrice, int iva) {
         this.productCode = productCode;
         this.productName = productName;
